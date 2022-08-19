3abd85f28422ffff97e412328dcc7609e7aa18a2
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2022-08-19 10:23:49 +0700

    oops, it is worng file

--- discord-api/config.py
+++ discord-api/config.py
@@ -1,6 +1,8 @@
-# Microbot Discord bot
-# Repo: https://github.com/tinelix/microbot
-# Licensed under Apache License 2.0 & GNU Affero General Public License v3.0 and higher
+# Den4ik Bot
+# Created by tretdm (aka. tinelix) at 2022-08-18 from Den4ik
+# Repo: https://github.com/den4ikbot/den4ikbot
+# Based on Microbot Discord bot: https://github.com/tinelix/microbot.
+# Licensed under Apache License v2.0 & GNU Affero General Public License v3.0 and higher.
 
 import os
 from dotenv import load_dotenv # loading environment variables module, for install 'pip install python-dotenv'
@@ -8,23 +10,23 @@ dotenv_path = os.path.join('../', '.env')
 
 tokens = {
     'discord_api': os.environ['DISCORD_TOKEN'],    # Discord API token from system environment
-    'owm_api': os.environ['OPENWEATHERMAP_TOKEN']  # for OpenWeatherMap API
 }
 
 config = {
-    'name': 'Microbot',
-    'version': '0.2.6',
+    'name': 'Den4ik Bot',
+    'version': '0.0.3',
     'version_date': '2022-08-19',
-    'prefix': '>',
+    'prefix': '+',
     'accent_def': 0x33b5e5,
     'accent_err': 0xff4444,
-    'dev_id': 741883312108339231, # Bot developer ID
-    'bugs_ch': 995275176029732894,  # To show a bug report in the console, set the value to 0
+    'dev_id': 951845579502280725, # Bot developer ID
+    'codev_id': 741883312108339231, # Bot codeveloper ID
+    'bugs_ch': 0,  # To show a bug report in the console, set the value to 0
 }
 
 links = {
-    'invite': 'https://discord.com/api/oauth2/authorize?client_id=994906248526970951&permissions=2147862592&scope=bot',
-    'support': 'https://discord.gg/saEHAWzYt3',
-    'website': 'https://tinelix.github.io',
-    'repo': 'https://github.com/tinelix/microbot',
+    'invite': 'https://discord.com/api/oauth2/authorize?client_id=1009762625158127636&permissions=2147862592&scope=bot',
+    'support': 'https://discord.gg/fHStmPJ35W',
+    'website': '',
+    'repo': 'https://github.com/den4ikbot/den4ikbot',
 }
